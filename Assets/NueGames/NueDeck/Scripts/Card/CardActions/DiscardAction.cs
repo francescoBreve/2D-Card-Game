8fc49a24587f278faf2ce0d6a54be539a6a66aad
Francesco <francescobreve@gmail.com>
2025-03-13 15:21:02 +0100

    Project Init

---
+++ Assets/NueGames/NueDeck/Scripts/Card/CardActions/DiscardAction.cs
@@ -0,0 +1,28 @@
+﻿using NueGames.NueDeck.Scripts.Enums;
+using NueGames.NueDeck.Scripts.Managers;
+using UnityEngine;
+
+namespace NueGames.NueDeck.Scripts.Card.CardActions
+{
+    public class DiscardAction : CardActionBase
+    {
+        public override CardActionType ActionType => CardActionType.Discard;
+        public override void DoAction(CardActionParameters actionParameters)
+        {
+            if (CollectionManager != null)
+                for(int i = 0; i < Mathf.RoundToInt(actionParameters.Value); i++){
+                    CollectionManager.DiscardCard();
+                }
+                
+            else
+                Debug.LogError("There is no CollectionManager");
+
+            if (FxManager != null)
+                FxManager.PlayFx(actionParameters.SelfCharacter.transform, FxType.Buff);
+
+            if (AudioManager != null)
+                AudioManager.PlayOneShot(actionParameters.CardData.AudioType);
+                
+        }
+    }
+}
